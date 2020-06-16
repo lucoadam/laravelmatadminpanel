6a67d0f22a8c288958d12b12f9c6156a96616075
Dipak Shrestha <dipakshrestha1288@gmail.com>
2020-06-16 14:10:44 +0545

    added other component of map in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -850,6 +850,43 @@
       // md.initDashboardPageCharts();
       <!-- javascript for init -->
 
+       /* var mapData = {
+            "AU": 760,
+            "BR": 550,
+            "CA": 120,
+            "DE": 1300,
+            "FR": 540,
+            "GB": 690,
+            "GE": 200,
+            "IN": 200,
+            "RO": 600,
+            "RU": 300,
+            "US": 2920,
+        };
+
+        $('#worldMap').vectorMap({
+            map: 'world_mill_en',
+            backgroundColor: "transparent",
+            zoomOnScroll: false,
+            regionStyle: {
+                initial: {
+                    fill: '#e4e4e4',
+                    "fill-opacity": 0.9,
+                    stroke: 'none',
+                    "stroke-width": 0,
+                    "stroke-opacity": 0
+                }
+            },
+
+            series: {
+                regions: [{
+                    values: mapData,
+                    scale: ["#AAAAAA", "#444444"],
+                    normalizeFunction: 'polynomial'
+                }]
+            },
+        });*/
+
         // We break the cards headers if there is too much stress on them :-)
         $('[data-header-animation="true"]').each(function() {
             var $fix_button = $(this)
@@ -1044,5 +1081,6 @@
         ]
         });
     });
+
   </script>
 @endpush
